f56ef3632db16eb0a20502329397889b8e8acafc
FRERE Thibaud <frere.thibaud@gmail.com>
2025-01-14 10:49:31 +0100

    update

--- server/server.py
+++ server/server.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://recipes.tfrere.com",
-        "http://recipes.tfrere.com"
+        "http://recipes.tfrere.com",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
