f902f2a54f971d85a8eb644e84f284d13c7c4647
FRERE Thibaud <frere.thibaud@gmail.com>
2025-01-14 10:29:48 +0100

    update

--- server/server.py
+++ server/server.py
@@ -17,7 +17,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://localhost:5173",
         "https://recipes.tfrere.com",
         "http://recipes.tfrere.com"
     ],
